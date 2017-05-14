e3a4a2352b4181eb9be9ae1d62545bec87b53164
surewinter <surewinter@163.com>
2017-05-14 11:45:06 +0800

    多媒体

---
+++ MyApplication/app/src/main/java/com/example/luofeiyu/listviewtest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.luofeiyu.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String[] data = {"111","2222","3333","444","655","777","990909090"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
+
+
+public class Fruit {
+    private String name;
+    private  int imageId;
+    public  Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+}
+
+
---
+++ NotificationTest/app/src/main/java/com/example/luofeiyu/notificationtest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.luofeiyu.notificationtest;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendNotice = (Button) findViewById(R.id.send);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send:
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new NotificationCompat.Builder(this).setContentTitle("你中大奖了")
+                        .setContentText("免费得iPhone7")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .build();
+                manager.notify(1,notification);
+                break;
+            default:
+                break;
+        }
+    }
+}
