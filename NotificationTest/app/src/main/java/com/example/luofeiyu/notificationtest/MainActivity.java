dde7863a88dc98e14d3b23c6d4a696e49a2583a2
surewinter <surewinter@163.com>
2017-05-15 16:06:37 +0800

    网络

--- NotificationTest/app/src/main/java/com/example/luofeiyu/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/luofeiyu/notificationtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.luofeiyu.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -24,14 +26,18 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.send:
+                Intent intent = new Intent(this, NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this).setContentTitle("你中大奖了")
                         .setContentText("免费得iPhone7")
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
                         .build();
                 manager.notify(1,notification);
+
                 break;
             default:
                 break;
